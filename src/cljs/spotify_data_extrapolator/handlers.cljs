f11ea91ca897ac556c85d5d1c0215d8b016e2580
Dexter Gramfors <dexter.gramfors@gmail.com>
2016-06-26 09:42:58 +0200

    Nicer handlers.

--- src/cljs/spotify_data_extrapolator/handlers.cljs
+++ src/cljs/spotify_data_extrapolator/handlers.cljs
@@ -1,5 +1,5 @@
 (ns spotify-data-extrapolator.handlers
-  (:require [re-frame.core :refer [register-handler dispatch]]
+  (:require [re-frame.core :refer [register-handler dispatch trim-v]]
             [spotify-data-extrapolator.db :as db]
             [spotify-data-extrapolator.api :as api]
             [clojure.string :as str]
@@ -10,6 +10,48 @@
   (map #(-> (select-keys % [:name :id])
             (assoc :image ((comp :url first :images) %))) artists))
 
+(defn set-active-panel-handler [db [active-panel]]
+  (assoc db :active-panel active-panel))
+
+(defn artist-search-changed-handler [db [artist-search]]
+  (dispatch [:get-artists artist-search])
+  db)
+
+(defn artists-response-handler [db [response]]
+  (let [clj-response (js->clj response)
+        items (get-in clj-response [:artists :items])
+        artists (artist-data items)]
+    (assoc db :artists artists)))
+
+(defn failed-response-handler [db [{:keys [status status-text]}]]
+  (log (str "Something went wrong: " status " " status-text))
+  db)
+
+(defn get-artists-handler [db [artist]]
+  (if-not (str/blank? artist)
+      (do (api/search-artists
+            artist
+            {:handler       #(dispatch [:artists-response %])
+             :error-handler #(dispatch [:failed-response %])})
+          db)
+      (dissoc db :artists)))
+
+(defn related-artists-response-handler [db [response]]
+  (let [clj-response (js->clj response)
+          items (clj-response :artists)
+          artists (artist-data items)]
+      (dispatch [:set-active-panel :inspired-by-panel])
+      (-> db
+          (dissoc :artists)
+          (assoc :inspired-by-artists artists))))
+
+(defn inspired-by-search-handler [db [id]]
+  (api/related-artists
+      id
+      {:handler       #(dispatch [:related-artists-response %])
+       :error-handler #(dispatch [:failed-response %])})
+  db)
+
 (register-handler
   :initialize-db
   (fn [_ _]
@@ -17,56 +59,35 @@
 
 (register-handler
   :set-active-panel
-  (fn [db [_ active-panel]]
-    (assoc db :active-panel active-panel)))
+  trim-v
+  set-active-panel-handler)
 
 (register-handler
   :artist-search-changed
-  (fn [db [_ artist-search]]
-    (dispatch [:get-artists artist-search])
-    db))
+  trim-v
+  artist-search-changed-handler)
 
 (register-handler
   :artists-response
-  (fn [db [_ response]]
-    (let [clj-response (js->clj response)
-          items (get-in clj-response [:artists :items])
-          artists (artist-data items)]
-      (assoc db :artists artists))))
+  trim-v
+  artists-response-handler)
 
 (register-handler
   :failed-response
-  (fn [db [_ {:keys [status status-text]}]]
-    (log (str "Something went wrong: " status " " status-text))
-    db))
+  trim-v
+  failed-response-handler)
 
 (register-handler
   :get-artists
-  (fn [db [_ artist]]
-    (if-not (str/blank? artist)
-      (do (api/search-artists
-            artist
-            {:handler       #(dispatch [:artists-response %])
-             :error-handler #(dispatch [:failed-response %])})
-          db)
-      (dissoc db :artists))))
+  trim-v
+  get-artists-handler)
 
 (register-handler
   :related-artists-response
-  (fn [db [_ response]]
-    (let [clj-response (js->clj response)
-          items (clj-response :artists)
-          artists (artist-data items)]
-      (dispatch [:set-active-panel :inspired-by-panel])
-      (-> db
-          (dissoc :artists)
-          (assoc :inspired-by-artists artists)))))
+  trim-v
+  related-artists-response-handler)
 
 (register-handler
   :inspired-by-search
-  (fn [db [_ id]]
-    (api/related-artists
-      id
-      {:handler       #(dispatch [:related-artists-response %])
-       :error-handler #(dispatch [:failed-response %])})
-    db))
+  trim-v
+  inspired-by-search-handler)
